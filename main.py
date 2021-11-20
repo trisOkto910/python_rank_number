bd9da441ccf02d79bd5c4f84e09ca5184afb9e2b
Tris <60920888+trisOkto910@users.noreply.github.com>
2021-11-20 16:09:03 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+def pangkat(a, b):
+    return (a ** b)
+
+a = int(input('Please enter the numbers : '))
+b = int(input('Please enter the rank number : '))
+
+print(f'pangkat of {a} and {b} is {pangkat(a, b)}')
