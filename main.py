b2a0f75d2e28468e8f92afd9de9d67620ce21667
Tris <60920888+trisOkto910@users.noreply.github.com>
2021-11-22 23:27:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 def pangkat(a, b):
-    return (a ** b)
+    hasil = a
+    
+    if b < 0:
+        
+        b = abs(b)
+        
+        return 1 / (hitungPow(a, b))
+    else:
+        for i in range(b - 1) :
+            hasil *= a    
+
+        return hasil
 
 a = int(input('Please enter the numbers : '))
 b = int(input('Please enter the rank number : '))
